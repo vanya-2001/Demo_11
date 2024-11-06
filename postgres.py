5b5418ab1c7c2c962176d22ed669a0baac03cbe8
ipapMaster <wmast@inbox.ru>
2024-11-06 12:04:34 +0300

    Offtop: postgres

--- postgres.py
+++ postgres.py
@@ -1,8 +1,8 @@
 # pip install psycopg2
 import psycopg2
 
-connection = psycopg2.connect(user="postgres", password="123",
-                              host="127.0.0.1", port="5432",
+connection = psycopg2.connect(user='postgres', password='123',
+                              host='127.0.0.1', port='5432',
                               database='lib')
 
 cursor = connection.cursor()
